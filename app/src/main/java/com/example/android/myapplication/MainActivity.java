a5de22a395d6bcf7be338676899c0ba12196cf83
Piyush Kumar Prusty <piyushprusty10@gmail.com>
2021-06-28 18:22:37 +0530

    Gesture Detect to all activity

--- app/src/main/java/com/example/android/myapplication/MainActivity.java
+++ app/src/main/java/com/example/android/myapplication/MainActivity.java
@@ -8,60 +8,87 @@ import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity implements GestureDetector.OnGestureListener,GestureDetector.OnDoubleTapListener {
+public class MainActivity extends AppCompatActivity implements GestureDetector.OnGestureListener, GestureDetector.OnDoubleTapListener {
 
 
     private TextView myMessage;
-    private GestureDetector gestureDetector;
+     GestureDetector gestureDetector;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        myMessage = (TextView) findViewById(R.id.mynam);
+        this.gestureDetector = new GestureDetector(this,this);
+        gestureDetector.setOnDoubleTapListener(this);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        this.gestureDetector.onTouchEvent(event);
+        return super.onTouchEvent(event);
     }
 
     @Override
     public boolean onSingleTapConfirmed(MotionEvent e) {
+        myMessage.setText("onSingleTapConfirmed ");
         return false;
     }
 
     @Override
     public boolean onDoubleTap(MotionEvent e) {
+        myMessage.setText("onDoubleTap");
+
         return false;
     }
 
     @Override
     public boolean onDoubleTapEvent(MotionEvent e) {
+        myMessage.setText("onDoubleTapEvent");
+
         return false;
     }
 
     @Override
     public boolean onDown(MotionEvent e) {
+        myMessage.setText("onDown");
+
         return false;
     }
 
     @Override
     public void onShowPress(MotionEvent e) {
+        myMessage.setText(" onShowPress");
+
 
     }
 
     @Override
     public boolean onSingleTapUp(MotionEvent e) {
+        myMessage.setText("onSingleTapUp");
+
         return false;
     }
 
     @Override
     public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+        myMessage.setText("onScroll");
+
         return false;
     }
 
     @Override
     public void onLongPress(MotionEvent e) {
+        myMessage.setText("onLongPress");
+
 
     }
 
     @Override
     public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+        myMessage.setText("onFling");
+
         return false;
     }
 }
